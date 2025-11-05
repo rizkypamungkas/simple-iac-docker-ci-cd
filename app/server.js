0ecd324e327834b331c30baf84201e07c85851e4
rizkypamungkas <rizkytripamungkas9@gmail.com>
2025-11-05 21:20:20 +0700

    fix workflows

--- app/server.js
+++ app/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('🚀 Hello from Node.js running in Docker on EC2!');
+app.get("/", (req, res) => {
+  res.send("🚀 Hello from Node.js running in Docker on EC2!");
 });
 
 app.listen(PORT, () => {
