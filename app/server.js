899567c484f6d90baee37f01a4c0b4b3f92b2d59
rizkypamungkas <rizkytripamungkas9@gmail.com>
2025-11-07 11:07:10 +0700

    update v1

--- app/server.js
+++ app/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Path ke folder html dan css
+const htmlPath = path.join(__dirname, "html");
+const cssPath = path.join(__dirname, "css");
+
+// Serve file statis (biar bisa akses /css/style.css dari browser)
+app.use(express.static(htmlPath));
+app.use("/css", express.static(cssPath));
+
+// Kirim index.html untuk route utama
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Node.js running in Docker on EC2!");
+  res.sendFile(path.join(htmlPath, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
